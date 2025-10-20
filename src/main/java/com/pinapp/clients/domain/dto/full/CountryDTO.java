f33f1b6f43d9fd4bab3d65a57125a6fa2c0b9fab
Juan Pablo Corvalan <juanpablocorvalan98@gmail.com>
2025-10-20 15:31:10 -0300

    fix: record to class

--- src/main/java/com/pinapp/clients/domain/dto/full/CountryDTO.java
+++ src/main/java/com/pinapp/clients/domain/dto/full/CountryDTO.java
@@ -1,13 +1,14 @@
 package com.pinapp.clients.domain.dto.full;
 
 import com.pinapp.clients.domain.dto.partial.ClientPartialDTO;
+import lombok.Data;
 
 import java.util.Set;
 
-public record CountryDTO(
-        Integer id,
-        String country,
-        String code,
-        Set<ClientPartialDTO> clients
-) {
+@Data
+public class CountryDTO {
+    private Integer id;
+    private String country;
+    private String code;
+    private Set<ClientPartialDTO> client;
 }
--- src/main/java/com/pinapp/clients/domain/dto/full/DocumentTypeDTO.java
+++ src/main/java/com/pinapp/clients/domain/dto/full/DocumentTypeDTO.java
@@ -1,12 +1,13 @@
 package com.pinapp.clients.domain.dto.full;
 
 import com.pinapp.clients.domain.dto.partial.ClientPartialDTO;
+import lombok.Data;
 
 import java.util.Set;
 
-public record DocumentTypeDTO(
-        Integer id,
-        String docType,
-        Set<ClientPartialDTO> clients
-) {
+@Data
+public class DocumentTypeDTO {
+    private Integer id;
+    private String docType;
+    private Set<ClientPartialDTO> client;
 }
